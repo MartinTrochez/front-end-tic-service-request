c6a44a11956520cc0535cf07d671b609c1dfae53
Fabio Denis <denisfabio44@gmail.com>
2025-11-04 13:27:44 -0300

    Conexión del historial de capacitaciones a la base de datos

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
 import { perfilRouter } from '@/modules/perfil/server/procedures';
+import { historialCapacitacionesRouter } from '@/modules/historial-capacitaciones/server/procedures';
 import { createTRPCRouter } from '../init';
 
 export const appRouter = createTRPCRouter({
     perfil: perfilRouter,
+    historialCapacitaciones: historialCapacitacionesRouter,
 });
 
 // export type definition of API
